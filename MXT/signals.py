16d8abdddf565252bb6ca50b928ff20b2ae97762
Josh Griffin <griffinj@oregonstate.edu>
2022-08-27 12:47:02 -0700

    functional autocorrelation est. plot

--- MXT/signals.py
+++ MXT/signals.py
@@ -11,12 +11,20 @@ def impulse_train_gen(A,N):
         i+=1
 
 
+def sin_gen(f,A,fs):
+    t = 0
+    dt = 1/fs
+    while 1:
+        yield(A*np.sin(f*t))
+        t+=dt
+
+
 def Rxx(X):
     R = np.zeros([X.shape[1],X.shape[1]])
     for i in range(X.shape[0]):
         for j in range(i,X.shape[0]):
-            R += X[j,:].T@X[i,:]
-    return(R)
+            R += X[j,:][:,None]@X[i,:][:,None].T
+    return(R/X.shape[0])
 
 def Rxx_gen(wg):
     for w in wg:
--- __test__.py
+++ __test__.py
@@ -32,15 +32,20 @@ from PLT.plt import *
 from MXT.window import *
 from MXT.signals import *
 
-ng      = normal_gen(0,1)
-itg     = impulse_train_gen(1,10)
-eg      = ewma_gen(itg,.5)
-wg      = window_gen(eg,30)
-pg      = plt_gen(wg,'Normal Series')
-hg      = hist_gen(pg,20,'Hist Normal')
-w2g     = window2_gen(wg,10,30)
-Rg      = Rxx_gen(w2g)
-p2g     = plt2_gen(Rg,'Rxx plt')
+fs      = 300
+N_R     = 30
+sf,sA   = (100,3)
+
+ng      = normal_gen(       0,  1                               )
+itg     = impulse_train_gen(1,  10                              )
+sg      = sin_gen(          sf, sA,             fs              )
+eg      = ewma_gen(         sg,.5                               )
+wg      = window_gen(       sg, N_R                             )
+pg      = plt_gen(          wg, 'Normal Series'                 )
+hg      = hist_gen(         pg, 20,             'Hist Normal'   )
+w2g     = window2_gen(      wg, 20,             N_R             )
+Rg      = Rxx_gen(          w2g                                 )
+p2g     = plt2_gen(         Rg, 'Rxx plt'                       )
 
 
 for h in p2g:
